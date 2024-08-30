e13dff0e8f55b615748ac7a1a25b9d45efb29eda
unknown <kreede929@gmail.com>
2024-08-30 08:53:24 +0500

    New js

--- java.js
+++ java.js
@@ -1,22 +1,13 @@
-var name = prompt('Ismingizni kiriting')
-console.log(`Mening ismim ${name}`)
+var age = +prompt('Yoshingiz nechchida')
 
-var age = +prompt('Yoshingizni Kiriting')
-console.log(`Mening yoshim ${age} da`)
-
-var misol1 = +prompt('Misolni yeching 6 + 3 = ?')
-console.log(`1- misol: 6 + 3 = 9! (Sizning javobingiz ${misol1})`)
-
-var misol2 = +prompt('Misolni yeching 20 - 5 = ?')
-console.log(`2- misol: 20 - 5 = 15! (Sizning javobingiz ${misol2})`)
-
-var misol3 = +prompt('Misolni yeching 15 * 2 = ?')
-console.log(`3- misol: 15 * 2 = 30! (Sizning javobingiz ${misol3})`)
-
-var misol4 = +prompt('Misolni yeching 8 / 4 = ?')
-console.log(`4- misol: 8 / 4 = 2! (Sizning javobingiz ${misol4})`)
-
-var misol5 = +prompt('Misolni yeching 10 % 3 = ?')
-console.log(`5- misol: 10 % 3 = 1! (Sizning javobingiz ${misol5})`)
-
-var console = alert('Javoblarni tekshirish uchun consoleni oching !!!')
+if (age >= 60) {
+	alert('Siz nafaqa yoshida ekansiz')
+} else if (age >= 50) {
+	alert('Siz yaqinda nafaqa yoshiga yetarkansiz')
+} else if (age >= 49) {
+	alert('Siz hali ishlashingiz kerak')
+} else if (age >= 19) {
+	alert('Siz ishlashingiz kerak')
+} else if (age <= 18) {
+	alert('Siz hali yoshsiz oqisingiz kerak')
+}
