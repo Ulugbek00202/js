69736bf9ea846b0221b52c9af46aa83262040509
unknown <kreede929@gmail.com>
2024-08-30 09:42:10 +0500

    new js

--- java.js
+++ java.js
@@ -11,3 +11,71 @@ if (age >= 60) {
 } else if (age <= 18) {
 	alert('Siz hali yoshsiz oqisingiz kerak')
 }
+
+var soat = +prompt('Vaqtni kiriting')
+
+if (soat === 1) {
+	alert('Soat ertalabki 1 boldi !')
+} else if (soat === 2) {
+	alert('Soat ertalabki 2 boldi !')
+} else if (soat === 3) {
+	alert('Soat ertalabki 3 boldi !')
+} else if (soat === 4) {
+	alert('Soat ertalabki 4 boldi !')
+} else if (soat === 5) {
+	alert('Soat ertalabki 5 boldi !')
+} else if (soat === 6) {
+	alert('Soat ertalabki 6 boldi !')
+} else if (soat === 7) {
+	alert('Soat ertalabki 7 boldi !')
+} else if (soat === 8) {
+	alert('Soat ertalabki 8 boldi !')
+} else if (soat === 9) {
+	alert('Soat ertalabki 9 boldi !')
+} else if (soat === 10) {
+	alert('Soat ertalabki 10 boldi !')
+} else if (soat === 11) {
+	alert('Soat ertalabki 11 boldi !')
+} else if (soat === 12) {
+	alert('Soat abedgi 12 boldi !')
+} else if (soat === 13) {
+	alert('Soat abedgi 1 boldi !')
+} else if (soat === 14) {
+	alert('Soat abedgi 2 boldi !')
+} else if (soat === 15) {
+	alert('Soat abeddan keyingi 3 boldi !')
+} else if (soat === 16) {
+	alert('Soat abeddan keyingi 4 boldi !')
+} else if (soat === 17) {
+	alert('Soat abeddan keyingi 5 boldi !')
+} else if (soat === 18) {
+	alert('Soat shom payti 6 boldi !')
+} else if (soat === 19) {
+	alert('Soat shom vaxti 7 boldi !')
+} else if (soat === 20) {
+	alert('Soat shomdan keyin 8 boldi !')
+} else if (soat === 21) {
+	alert('Soat kechasi 9 boldi !')
+} else if (soat === 22) {
+	alert('Soat kechasi 10 boldi !')
+} else if (soat === 23) {
+	alert('Soat kechasi 11 boldi !')
+} else if (soat === 24) {
+	alert('Soat kechasi 12 boldi !')
+} else {
+	alert(
+		"Bunday turdagi vaqtlar o'ylab topilmagan hali ! Shuning uchun kalleni ishlat"
+	)
+}
+
+// var oneNumber = +prompt('Son kiriting')
+// var twoNumber = +prompt('Son kiriting')
+// var threeNumber = +prompt('Son kiriting')
+
+// if (oneNumber > twoNumber < threeNumber) {
+// 	alert(twoNumber)
+// } else if (twoNumber > oneNumber < threeNumber) {
+// 	alert(oneNumber)
+// } else if (oneNumber > threeNumber < twoNumber) {
+// 	alert(threeNumber)
+// }
